b9b2edf66ef88424b0dcb702fff28a19bcd0b302
Olya\Оля <olya_klim@ukr.net>
2018-08-28 16:04:07 +0300

    Add not finished cart-modal

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,25 @@ $('.slider').slick({
   prevArrow : "<button class='slider__button slider__button--prev'><svg class='slider__button-icon' width='10' height = '10'><use xlink:href='#icon_arrow_left'></use></svg></button>", 
   nextArrow : "<button class='slider__button slider__button--next'><svg class='slider__button-icon' width='10' height = '10'><use xlink:href='#icon_arrow_right'></use></svg></button>"
 });
+
+$('.vertical-categories__item').hover(function () {
+	var submenu = $(this).children("ul");
+	submenu.slideToggle();
+});
+
+var currentQuantity = 1;
+    $('.quantity-switch__less').on('click', function (e) {
+        e.preventDefault();
+        if (currentQuantity > 1) {
+            currentQuantity -= 1;
+            $(".quantity-switch__number").val(currentQuantity);
+        }
+    });
+    
+    $('.quantity-switch__more').on('click', function (e) {
+        e.preventDefault();
+        if(currentQuantity <= 9){
+        currentQuantity += 1;
+        $(".quantity-switch__number").val(currentQuantity);
+        }
+    });
