298312d2d7a050128a0f5cf9e3a5107d5e33ac35
Olya\Оля <olya_klim@ukr.net>
2018-08-29 11:25:17 +0300

    Finished call popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,32 @@
+var cartModal = $(".cart-modal");
+var callPopUp = $(".call-popup");
+var fancybox = $(".fancybox");
+
 $('.vertical-categories__item').hover(function() {
     var submenu = $(this).children("ul");
     submenu.toggle();
 });
 
 
-var cartModal = $(".cart-modal");
+
 $(document).mouseup(function(e) {
 
     if (!cartModal.is(e.target) &&
         cartModal.has(e.target).length === 0) {
-        cartModal.hide();
+        cartModal.slideUp();
     }
 });
 
 $('.cart__open-btn').click(function() {
-    cartModal.show();
+    cartModal.slideDown();
+});
+
+$('.button-request-call').click(function () {
+	fancybox.slideDown();
+	callPopUp.slideDown();
+});
+
+$(".call-popup__close-button").click(function () {
+	callPopUp.fadeOut();
+	fancybox.fadeOut();
 });
