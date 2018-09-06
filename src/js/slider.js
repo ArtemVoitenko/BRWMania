bf687f07d6172c49f125b610fba20976f80cf838
Olya\Оля <olya_klim@ukr.net>
2018-09-06 18:15:51 +0300

    start response

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,36 @@
 $('.slider').slick({
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     arrows: true,
-    variableWidth: true,
+    // variableWidth: true,
     prevArrow: "<button class='slider__button slider__button--prev'><svg class='slider__button-icon' width='10' height = '10'><use xlink:href='#icon_arrow_left'></use></svg></button>",
-    nextArrow: "<button class='slider__button slider__button--next'><svg class='slider__button-icon' width='10' height = '10'><use xlink:href='#icon_arrow_right'></use></svg></button>"
+    nextArrow: "<button class='slider__button slider__button--next'><svg class='slider__button-icon' width='10' height = '10'><use xlink:href='#icon_arrow_right'></use></svg></button>",
+    responsive: [
+    {
+      breakpoint: 1320,
+      settings: {
+        slidesToShow: 4
+
+      }
+  },
+      {
+      breakpoint: 1050,
+      settings: {
+        slidesToShow: 3
+      }
+      },
+            {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2
+      }
+      },
+    ]
 });
+$('.banner').slick({
+	slidesToShow: 1,
+	autoplay: true,
+	fade: true,
+	arrows: false,
+	respondTo: "slider"
+})
